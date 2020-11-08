b5bbe04f356a85173b8631d4dab03509e8564567
a00x40 <ahmedattia621@yahoo.com>
2020-11-09 01:20:30 +0200

    Finished Lab3 Median & Gaussian Filters

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -5,9 +5,51 @@
 '''
 (2) Median Filter
 '''
+def median_filter(image, window_width , window_height):
+    outImage = np.zeros( (len(image),len(image[0])))
+    
+    edgex = (window_width // 2) 
+    edgey = (window_height // 2) 
+    
+    for x in range (edgex , len(image) - edgex) :
+    
+        for y in range (edgex , len(image[0]) - edgey) :
+            
+            colorArray  = np.zeros( ( window_width , window_height ) )
+            
+            for fx in range( window_width ) :
+                
+                for fy in range( window_height ) :
+                    
+                    colorArray[fx][fy] = image[x + fx - edgex][y + fy - edgey]
+                    
+            colorArray.sort()
+            outImage[x][y] = colorArray[ window_width // 2][ window_height // 2]
+        
+    return outImage
+
+'''
+ Requirement 
+'''
+pout = io.imread("pout.tif")
+grayPout = rgb2gray(pout)
+
+# Salt & Pepper Noise
+noisePout = random_noise(grayPout ,mode="s&p",amount = 0.05)
+show_images([pout , grayPout  , noisePout] , ['Original','GrayScale','Noise'])
+
+# Median Filter
+my_filtered = median_filter( noisePout , 3 , 3 )
+
+skimedian = median( noisePout )
+show_images([my_filtered , skimedian] , ['My Filtered','Skimage Median Filtered'])
+
+# Gaussain Filter
+skiguassian1 = gaussian( noisePout , sigma=[8,3] )
+skiguassian2 = gaussian( noisePout , sigma=[6,5] )
+skiguassian3 = gaussian( noisePout , sigma=[3,2] )
+skiguassian4 = gaussian( noisePout , sigma=3 )
 
 
+show_images([skiguassian1,skiguassian2,skiguassian3,skiguassian4] , ['Skimage Gaussian Filter S=8,3','Skimage Gaussian Filter S=6,5','Skimage Gaussian Filter S=3,2','Skimage Gaussian Filter S=3'])
 
-pyramids = io.imread('pyramids.jpeg')
-greyPyramids = rgb2gray(pyramids)
-noiseImage = random_noise(greyPyramids,mode="s&p",amount = 0.05)
